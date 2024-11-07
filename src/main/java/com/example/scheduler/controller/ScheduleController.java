3d347618733c22da26f0a3aa0aafe8bf72752652
Vaxee03 <012jas84@gmail.com>
2024-11-07 20:02:29 +0900

    Feat : searchScheduleById 메서드 생성
    Id를 입력하여 해당하는 스케줄을 조회하는 메서드 생성
    및 ScheduleController 전체 주석 수정

--- src/main/java/com/example/scheduler/controller/ScheduleController.java
+++ src/main/java/com/example/scheduler/controller/ScheduleController.java
@@ -6,6 +6,7 @@ import com.example.scheduler.entity.Schedule;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class ScheduleController {
 
     private final Map<Integer, Schedule> schedules = new HashMap<>();
 
-    //스케줄 생성
+    // 스케줄 생성
     @PostMapping
     public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto dto) {
 
@@ -42,26 +43,42 @@ public class ScheduleController {
         // Inmemory DB에 Schedule을 저장
         schedules.put(scheduleId, Schedule);
 
-        //생성 성공 반환
+        // 생성 성공 반환
         return new ResponseEntity<>(new ScheduleResponseDto(Schedule), HttpStatus.CREATED);
     }
 
 
-    //스케줄 전체 조회
+    // 스케줄 전체 조회
     @GetMapping
     public List<ScheduleResponseDto> searchAllSchedules() {
 
-        //조회 리스트 생성
+        // 조회 리스트 생성
         List<ScheduleResponseDto> responseList = new ArrayList<>();
 
-        //조회 리스트에 스케줄 데이터 넣기
+        // 조회 리스트에 스케줄 데이터 넣기
         for(Schedule schedule : schedules.values()) {
             ScheduleResponseDto responseDto = new ScheduleResponseDto(schedule);
             responseList.add(responseDto);
         }
 
-        //조회 리스트 반환
+        // 조회 리스트 반환
         return responseList;
+    }
+
+    // ID로 스케줄 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<ScheduleResponseDto> searchScheduleById(@PathVariable Long id) {
+
+        // 입력한 Id키에 해당하는 데이터를 가져옴
+        Schedule schedule = schedules.get(id);
+
+        // NULL 값 여부 체크
+        if(schedule == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // 조회 스케줄 반환
+        return new ResponseEntity<>(new ScheduleResponseDto(schedule), HttpStatus.OK);
     }
 
 }
