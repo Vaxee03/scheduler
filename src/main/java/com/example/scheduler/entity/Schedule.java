8e4917e0c1c80098c0ea2cf4dbd95a6eb35e85c7
Vaxee03 <012jas84@gmail.com>
2024-11-07 16:57:26 +0900

    Feat : Schedule Class 생성
    각 Dto를 통해 데이터를 주고받는 Schedule 객체 클래스 생성

---
+++ src/main/java/com/example/scheduler/entity/Schedule.java
@@ -0,0 +1,31 @@
+package com.example.scheduler.entity;
+
+import com.example.scheduler.dto.ScheduleRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.time.LocalDate;
+
+
+@Getter
+@AllArgsConstructor
+public class Schedule {
+
+    private Integer id;
+    private String title;
+    private String description;
+    private Integer password;
+    private LocalDate startPeriod;
+
+    public void updateTitle(ScheduleRequestDto dto) {
+        this.title = dto.getTitle();
+    }
+
+    public void updateDescription(ScheduleRequestDto dto) {
+        this.description = dto.getDescription();
+    }
+
+    public void update(ScheduleRequestDto dto) {
+        this.title = dto.getTitle();
+        this.description = dto.getDescription();
+    }
+}
